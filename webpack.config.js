2b4c93d5b01a926712a0c9d2cb1409ba37ebd0e6
Eira Teufel <sv.tolstyakova@gmail.com>
2020-02-08 14:36:40 +0300

    настрока webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+const path = require('path');
+// подключаем path к конфигу вебпак
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    }
+}
+// переписали точку выхода, используя утилиту path
